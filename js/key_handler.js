9d386fd4b996a0c067e9d82eb49ebb6edab537c7
lubenard <lubenard@student.42.fr>
2020-05-26 15:14:59 +0200

    Added basic web page + basic working repeat, save and print functions

---
+++ js/key_handler.js
@@ -0,0 +1,64 @@
+var letter_size = 0;
+var letter_array = [];
+var number;
+
+function save_localStorage_key(letter_index) {
+     // Because localStorage is key -> value, we also register it's timestamp
+     localStorage.setItem(letter_index, Date.now());
+     console.log(localStorage.getItem(letter_index));
+}
+ 
+function print_key_console(keynum) {
+     console.log("Key pressed: ", keynum);
+}
+
+function get_key(key) {
+    // We print the key
+    print_key_console(key.key);
+    // Save it into localStorage
+    letter_array.push(key.key);
+    save_localStorage_key(letter_size++);
+    console.log("Letter_size", letter_size);
+}
+
+function get_delay(i) {
+    console.log("i vaut ", i);
+        console.log("J'attend ", Number(localStorage.getItem(i) - localStorage.getItem("start_timer")));
+        return Number(localStorage.getItem(i) - localStorage.getItem("start_timer"));
+}
+
+function set_delay(i, delay) {
+    console.log("delay for ", letter_array[i] ," is ", localStorage.getItem(i));
+    var tempo = setTimeout(function(){ 
+        console.log("array is ",letter_array[i]);
+    }, delay);
+}
+
+function replay() {
+    var i = 0;
+    
+    console.log("------REPLAY-----");
+    console.log("letter array is ", letter_array);
+    for (i = 0; i < letter_array.length; i++)
+    { 
+        set_delay(i, get_delay(i));
+    }
+    //localStorage.clear();
+}
+
+function get_onclick_timestamp() {
+    localStorage.removeItem("start_timer");
+    // We check if it already exist in localStorage.
+    // If not, we store it, and if it already exist, we use it as start.
+    // The end timer will be the last key timestamp
+    if (!(localStorage.getItem("start_timer")))
+    {
+        console.log("début timestamp ", Date.now());
+        localStorage.setItem("start_timer", Date.now());
+    }
+    else
+    {  
+        console.log("timestamp actuel", Date.now());
+        console.log("OUPS, Timestamp déjà mis a", localStorage.getItem("start_timer"));
+    }
+}
