cad9f7591ad697f42ed3ceb001cd338fbb97456c
lubenard <lubenard@student.42.fr>
2020-05-28 17:51:40 +0200

    added loading bar + reset each time reload

--- js/key_handler.js
+++ js/key_handler.js
@@ -1,6 +1,10 @@
 var letter_size = 0;
 var letter_array = [];
 
+window.onload = function(){
+    document.getElementById("key_type").value = "";
+}
+
 function save_localStorage_key(letter_index) {
      // Because localStorage is key -> value, we also register it's timestamp
      localStorage.setItem(letter_index, Date.now());
@@ -17,10 +21,28 @@ function clear_textbox() {
     letter_size = 0;
 }
 
+function pb_move() {
+    var i = 1;
+    var elem = document.getElementById("bar_progress");
+    var width = 1;
+    var id = setInterval(pb_frame, 10);
+    function pb_frame() {
+        if (width >= 100) {
+            clearInterval(id);
+            i = 0;
+        } else {
+            width++;
+            elem.style.width = width + "%";
+        }
+    }
+}
+
 function reset(){
         clear_textbox();
         localStorage.removeItem("start_timer");
+        localStorage.removeItem("end_timer");
         document.getElementById("repeat_area").innerHTML = "";
+        document.getElementById("bar_progress").style.width =  "1%";
 }
 
 function get_key(key) {
@@ -30,7 +52,7 @@ function get_key(key) {
     letter_array.push(key.key);
     // Save it into localStorage. We save a index and not the letter to avoid overwrite with the same letters
     save_localStorage_key(letter_size++);
-    //we check if the key is the first in the string and if start_timer is not set_delay
+    //we check if the key is the first in the string and if start_timer is not set
     if (letter_array.lenght == 0 && !(localStorage.getItem("start_timer")))
     {
         set_start_time(localStorage.getItem("0"));
@@ -39,13 +61,10 @@ function get_key(key) {
 }
 
 function get_delay(i) {
-    console.log("i vaut ", i);
-        console.log("J'attend ", Number(localStorage.getItem(i) - localStorage.getItem("start_timer")));
-        return Number(localStorage.getItem(i) - localStorage.getItem("start_timer"));
+    return Number(localStorage.getItem(i) - localStorage.getItem("start_timer"));
 }
 
 function set_delay(i, delay) {
-    
     console.log("delay for ", letter_array[i] ," is ", delay);
     var tempo = setTimeout(function(){ 
         console.log("array is ",letter_array[i]);
@@ -57,12 +76,29 @@ function replay() {
     var i = 0;
     
     console.log("------REPLAY-----");
-    console.log("letter array is ", letter_array);
     for (i = 0; i < letter_array.length; i++)
     { 
-        set_delay(i, get_delay(i));
+        if (localStorage.getItem("end_timer") && localStorage.getItem(i) < localStorage.getItem("end_timer"))
+            set_delay(i, get_delay(i));
     }
-    //localStorage.clear();
+    pb_move();
+}
+
+function set_end_time(start_time) {
+    localStorage.removeItem("end_timer");
+    // We check if it already exist in localStorage.
+    // If not, we store it, and if it already exist, we use it as start.
+    // The end timer will be the last key timestamp
+    if (!(localStorage.getItem("end_timer")))
+    {
+        console.log("fin timestamp ", Date.now());
+        localStorage.setItem("end_timer", Date.now());
+    }
+    else
+    {  
+        console.log("timestamp actuel", Date.now());
+        console.log("OUPS, end timestamp déjà mis a", localStorage.getItem("end_timer"));
+    }
 }
 
 function set_start_time(start_time) {
